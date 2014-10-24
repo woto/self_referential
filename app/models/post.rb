b3df23794e6688cd1c1e57339e1f209257f54e6a
Ruslan Kornev <oganer@gmail.com>
2014-10-24 12:42:08 +0400

    Пример.
    Модели Author -> Post
    Author.create.posts.published.create - Работает.
    
    Модель Task завязанная на саму себя с помощью Self Referential
    Task.create.tasks.published.create - Не работает. Почему?
    Task.create.tasks.create - Работает.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
   belongs_to :author
+
+  scope :published, -> { where(status: 1) }
+
+  after_create do
+    puts '========================================='
+    puts "author_id is present: '#{author_id}'"
+    puts "what about task association? '#{author}'"
+    puts '========================================='
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,13 @@
 class Task < ActiveRecord::Base
   belongs_to :task
+  has_many :tasks
+
+  scope :published, -> { where(status: 1) }
+
+  after_create do
+    puts '========================================='
+    puts "task_id is present: '#{task_id}'"
+    puts "what about task association? '#{task}'"
+    puts '========================================='
+  end
 end
